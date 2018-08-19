c2996e4196ce0fb11e8334b219597f45e4df4e4f
JalexDooo <393351322@qq.com>
2018-08-19 13:29:34 +0800

    Image io.

--- test/__init__.py
+++ test/__init__.py
@@ -94,23 +94,24 @@ def test5():
 
 
 def test6():
-    filename = os.listdir(path)
+    filenames = os.listdir(path)
     writer = tf.python_io.TFRecordWriter("src/train.tfrecords")
-
-    for name in os.listdir(path):
-        class_path = path + os.sep + name
-        for img_name in os.listdir(class_path):
-            img_path = class_path + os.sep + img_name
-            img = Image.open(img_path)
-            img = img.resize((500, 500))
-            img_raw = img.tobytes()
-            example = tf.train.Example(features=tf.train.Features(
-                feature={
-                    "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[name])),
-                    'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
-                }
-            ))
-            writer.write(example.SerializeToString())
+    sep = ['jpg', 'png', 'jpeg']
+    for name in filenames:
+        img_path = path + os.sep + name
+        img_name, img_sep = name.split('.')
+        if img_sep not in sep:
+            continue
+        img = Image.open(img_path)
+        img = img.resize((500, 500))
+        img_raw = img.tobytes()
+        example = tf.train.Example(features=tf.train.Features(
+            feature={
+                "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[0])),
+                'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+            }
+        ))
+        writer.write(example.SerializeToString())
     return
 
 
