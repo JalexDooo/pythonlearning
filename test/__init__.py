61070dbcae0dbd5ac898a8eb792c84b4d72b88f5
JalexDooo <393351322@qq.com>
2018-09-05 18:53:41 +0800

    Python learning.

--- test/__init__.py
+++ test/__init__.py
@@ -1,31 +1,38 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 
-threshold = 1.0e-2
-x_data = np.random.randn(100).astype(np.float32)
-y_data = x_data * 0.3 + 0.15
+houses = 3
+features = 2
 
-weight = tf.Variable(1.)
-bias = tf.Variable(1.)
-x_ = tf.placeholder(tf.float32)
-y_ = tf.placeholder(tf.float32)
-y_model = tf.add(tf.multiply(x_, weight), bias)
+# 2*x1+3*x2
+x_data = np.zeros([houses, 2])
+for house in range(houses):
+    x_data[house, 0] = np.round(np.random.uniform(50., 150.))
+    x_data[house, 1] = np.round(np.random.uniform(3., 7.))
+weights = np.array([[2.], [3.]])
+y_data = np.dot(x_data, weights)
 
-loss = tf.reduce_mean(pow((y_model - y_), 2))
+print(x_data)
+print(y_data)
+
+x_ = tf.placeholder(tf.float32, [None, 2])
+y_ = tf.placeholder(tf.float32, [None, 1])
+weights_ = tf.Variable(np.ones([2, 1]), dtype=tf.float32)
+y_model = tf.matmul(x_, weights_)
+
+print("weights: ", weights_)
+
+loss = tf.reduce_mean(tf.pow(y_model - y_, 2))
 train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-flag = True
-while flag:
-    for (x, y) in zip(x_data, y_data):
-        sess.run(train_op, feed_dict={x_: x, y_: y})
-        print('weight={}|bias={}'.format(weight.eval(sess), bias.eval(sess)))
-    if sess.run(loss, feed_dict={x_: x_data, y_: y_data}) <= threshold:
-        flag = False
-plt.plot(x_data, y_data, 'ro', label='Original data')
-plt.plot(x_data, sess.run(weight) * x_data + sess.run(bias), label='Fitted line')
-plt.legend()
-plt.show()
+#for _ in range(10):
+for x, y in zip(x_data, y_data):
+    z1 = x.reshape(1, 2)
+    z2 = y.reshape(1, 1)
+    print("z1: ", z1)
+    print("z2: ", z2)
+    sess.run(train_op, feed_dict={x_: z1, y_: z2})
+    print(weights_.eval(sess))
